5c7e36ecb0958ea258b69016276f5a136525ff5a
qianyong <qianyong@jd.com>
2016-06-12 15:48:08 +0800

    add unit test to NotebookAuthorization

---
+++ zeppelin-zengine/src/test/java/org/apache/zeppelin/notebook/NotebookAuthorizationTest.java
@@ -0,0 +1,21 @@
+package org.apache.zeppelin.notebook;
+
+import org.apache.zeppelin.conf.ZeppelinConfiguration;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NotebookAuthorizationTest {
+
+    NotebookAuthorization noteAuth = new NotebookAuthorization(ZeppelinConfiguration.create());
+
+    @Test
+    public void testSaveToFile() {
+        Set<String> entities = new HashSet<String>();
+        entities.add("user1");
+        entities.add("user2");
+        noteAuth.setOwners("r", entities);
+        noteAuth.setOwners("2A94M5J1Z", entities);
+    }
+}
