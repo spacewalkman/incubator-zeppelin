faba854c76c92a7e2f43e07f8e46b6fba4ae356f
qianyong <qianyong@jd.com>
2016-09-08 17:34:30 +0800

    统一日期格式，不再使用原来的fromZeppelinJson static方法

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/VFSNotebookRepo.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/VFSNotebookRepo.java
@@ -157,7 +157,7 @@ public class VFSNotebookRepo implements NotebookRepo {
     String json = IOUtils.toString(ins, conf.getString(ConfVars.ZEPPELIN_ENCODING));
     ins.close();
 
-    Note note = GsonUtil.fromZeppelinJson(json, Note.class);
+    Note note = GsonUtil.fromJson(json, Note.class);
 //    note.setReplLoader(replLoader);
 //    note.jobListenerFactory = jobListenerFactory;
 
