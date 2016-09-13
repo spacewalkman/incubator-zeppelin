20cf4e5e938d06ac8398e21f41460cc1ec016712
qianyong <qianyong@jd.com>
2016-09-13 09:39:30 +0800

    去掉interface中method前默认的public修饰符

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/NoteEventListener.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/NoteEventListener.java
@@ -22,7 +22,9 @@ import org.apache.zeppelin.scheduler.Job;
  * NoteEventListener
  */
 public interface NoteEventListener {
-  public void onParagraphRemove(Paragraph p);
-  public void onParagraphCreate(Paragraph p);
-  public void onParagraphStatusChange(Paragraph p, Job.Status status);
+  void onParagraphRemove(Paragraph p);
+
+  void onParagraphCreate(Paragraph p);
+
+  void onParagraphStatusChange(Paragraph p, Job.Status status);
 }
