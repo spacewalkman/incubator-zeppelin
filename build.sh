99c643b4009de945a37cd0c7a6a09064df3aa587
spacewalkman <spacewalkman@163.com>
2016-11-03 16:06:32 +0800

    在build过程中单独在zeppelin-web目录中执行一遍grunt build

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 mvn clean 
 cd zeppelin-web
 bower install --allow-root
+grunt build
 cd ..
 mvn  install -Pbuild-distr -Pspark-1.6 -Dspark.version=1.6.2 -Phadoop-2.7 -Dhadoop.version=2.7.2 -Psparkr -Ppyspark -Dmaven.findbugs.enable=false -Drat.skip=true -Dcheckstyle.skip=true -Denforcer.skip=true -Dcobertura.skip=true -DskipTests -X
