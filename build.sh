7142db2bc5c5742b42f8b05b4b89299b31ec28eb
spacewalkman <spacewalkman@163.com>
2016-11-03 15:27:18 +0800

    增加build脚本

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mvn clean 
+cd zeppelin-web
+bower install --allow-root
+cd ..
+mvn  install -Pbuild-distr -Pspark-1.6 -Dspark.version=1.6.2 -Phadoop-2.7 -Dhadoop.version=2.7.2 -Psparkr -Ppyspark -Dmaven.findbugs.enable=false -Drat.skip=true -Dcheckstyle.skip=true -Denforcer.skip=true -Dcobertura.skip=true -DskipTests -X
