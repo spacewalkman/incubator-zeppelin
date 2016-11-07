ee63b8f35469a7282caa5423286ff83475d8d52d
spacewalkman <spacewalkman@163.com>
2016-11-07 17:27:23 +0800

    通过mvn -pl编译时禁用到不用的interpreter，加快编译时间，提高安全性

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd zeppelin-web
 bower install --allow-root
 grunt build
 cd ..
-mvn  install -Pbuild-distr -Pspark-1.6 -Dspark.version=1.6.2 -Phadoop-2.7 -Dhadoop.version=2.7.2 -Psparkr -Ppyspark -Dmaven.findbugs.enable=false -Drat.skip=true -Dcheckstyle.skip=true -Denforcer.skip=true -Dcobertura.skip=true -DskipTests -X
+mvn  install -Pbuild-distr -Pspark-1.6 -Dspark.version=1.6.2 -Phadoop-2.7 -Dhadoop.version=2.7.2 -Psparkr -Ppyspark -Dmaven.findbugs.enable=false -Drat.skip=true -Dcheckstyle.skip=true -Denforcer.skip=true -Dcobertura.skip=true -DskipTests  -pl '!livy,!hbase,!postgresql,!flink,!ignite,!kylin,!lens,!cassandra,!elasticsearch,!bigquery,!alluxio' -X
